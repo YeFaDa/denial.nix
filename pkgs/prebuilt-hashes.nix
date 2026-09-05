611eaa8fc945837f0c9c2f0a835da18401435bbf
YeFaDa <261456766+YeFaDa@users.noreply.github.com>
2026-09-05 03:41:24 +0000

    chore: update Denial pins

--- pkgs/prebuilt-hashes.nix
+++ pkgs/prebuilt-hashes.nix
@@ -22,15 +22,15 @@ in
   "x86_64-linux" = {
     denial = {
       url = "https://github.com/denialwm/denial/releases/download/v${version}/denial-${version}-1-x86_64.pkg.tar.zst";
-      hash = "sha256-t2/BOuGrkNYi6w9mLSHhIGtKcXaRv+KHklzr2Jne5VY=";
+      hash = "sha256-oKI+C4UIIe9pd3nkLzDYpTeF1OwPV29LHV+CXwRCbQM=";
     };
     engine = {
       url = "https://github.com/denialwm/denial/releases/download/v${version}/denial-flutter-engine-1.${version}-1-x86_64.pkg.tar.zst";
-      hash = "sha256-V/PzHmD3e3XN3bQRghITkqTHb9m9YtJVcLSkijG0kUg=";
+      hash = "sha256-moyDvDyzMUTaxIHtBwSlYDzjiqBNczyElRzkSCM058w=";
     };
     uiDevelopment = {
       url = "https://github.com/denialwm/denial/releases/download/v${version}/denial-ui-development-${version}-1-x86_64.pkg.tar.zst";
-      hash = "sha256-1p300E0Stlr2P5Wp9eKohMOeIzt/J1TdSkj/SXL6siU=";
+      hash = "sha256-v35GRv9JYPbnzG6LsZFMkNzpdo7aL3x4MzTh1Vt86yw=";
     };
   };
 }
